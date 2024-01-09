b9d8c09dfc1d05e45f9c64994d716b379aa386c3
Van Dung Dev <vincentcode402112@gmail.com>
2024-01-09 19:55:43 +0700

    9/1/2024A

---
+++ ActuatorAndSecurity/src/main/java/com/dungken/ActuatorAndSecurity/ActuatorAndSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.dungken.ActuatorAndSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorAndSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorAndSecurityApplication.class, args);
+	}
+
+}
