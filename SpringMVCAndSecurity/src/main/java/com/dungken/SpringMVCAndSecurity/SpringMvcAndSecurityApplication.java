f608cc855b0ededd7258e6d67f0658887d472065
Van Dung Dev <vincentcode402112@gmail.com>
2024-01-21 10:49:34 +0700

    21/1/2024M

---
+++ SpringMVCAndSecurity/src/main/java/com/dungken/SpringMVCAndSecurity/SpringMvcAndSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.dungken.SpringMVCAndSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcAndSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcAndSecurityApplication.class, args);
+	}
+
+}
---
+++ SpringMVCAndSecurity/src/test/java/com/dungken/SpringMVCAndSecurity/SpringMvcAndSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dungken.SpringMVCAndSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcAndSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
